7a69f3125dac33ec87c0730a03ae5f9465b909d9
Goous1337 <127313390+Goous1337@users.noreply.github.com>
2024-03-05 19:03:33 +0300

    create add enums for client

---
+++ src/main/java/ru/jpetstoretest/model/FavouriteType.java
@@ -0,0 +1,21 @@
+package ru.jpetstoretest.model;
+
+public enum FavouriteType {
+
+    FISH("FISH"),
+    DOGS("DOGS"),
+    REPTILES("REPTILES"),
+    CATS("CATS"),
+    BIRDS("BIRDS");
+
+    private final String value;
+
+    FavouriteType(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+}
---
+++ src/main/java/ru/jpetstoretest/model/LanguageType.java
@@ -0,0 +1,16 @@
+package ru.jpetstoretest.model;
+
+public enum LanguageType {
+
+   ENGLISH("english"),
+   JAPANESE("japanese");
+
+    private final String value;
+
+    LanguageType(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
