4295583b7dfb5ac7f7bf30f27155ef77611fd3bb
v.grausov <v.grausov@astondevs.ru>
2025-02-26 20:58:41 +0300

    fix ci test

--- src/test/java/ru/jpetsroretest/order/CreateOrderTest.java
+++ src/test/java/ru/jpetsroretest/order/CreateOrderTest.java
@@ -9,19 +9,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.jpetstoretest.model.CardType;
 import ru.jpetstoretest.model.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Feature("Проверка функциональности заказов")
 public class CreateOrderTest extends BaseUiTest {
 
+    public static final Logger logger = LoggerFactory.getLogger(CreateOrderTest.class);
+
     @Test
     @Story("Пользователь может создать заказ")
     @DisplayName("Создание заказа")
-    public void createOrderTest () {
+    public void createOrderTest() {
+
+        logger.info("Начало теста: создание заказа");
+
         CardType cardType = CardType.AMERICAN_EXPRESS;
+
+        logger.debug("Переход на страницу логина");
         baseRouter.loginPage()
                 .clickSignInButton()
                 .clickRegisterNowButton();
+
+        logger.debug("Регистрация нового клиента");
         Client client = registerNewClient();
+
+        logger.debug("Авторизация клиента");
         baseRouter.mainPage()
                 .clickSingInButton();
         baseRouter.loginPage()
@@ -29,14 +42,21 @@ public class CreateOrderTest extends BaseUiTest {
                 .sendKeysPasswordInputField(client.getPassword())
                 .clickLoginButton();
 
+        logger.debug("Выбор категории Fish");
         baseRouter.mainPage()
                 .clickCategoryFishButton();
         baseRouter.fishCategoryPage()
                 .clickAngelFishButton();
+
+        logger.debug("Добавление товара в корзину");
         baseRouter.productPage()
                 .clickAddCartButton();
+
+        logger.debug("Переход к оформлению заказа");
         baseRouter.shoppingCartPage()
                 .clickCheckoutButton();
+
+        logger.debug("Заполнение данных карты");
         baseRouter.cardRegistrationPage()
                 .clickTypeDropdownButton()
                 .clickCardTypeValueButton(cardType)
@@ -44,9 +64,13 @@ public class CreateOrderTest extends BaseUiTest {
                 .clickContinueButton()
                 .clickAccountInformationButton();
 
+        logger.debug("Проверка успешного создания заказа");
         baseRouter.cardRegistrationPage()
                 .orderSuccessfullyCreatedTextShouldExist();
-    }
+
+        logger.info("Тест завершен");
+
+}
 
     @Test
     @Story("Пользователь может менять количесвто товаров в заказе")
