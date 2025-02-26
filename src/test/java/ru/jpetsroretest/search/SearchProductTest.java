f8e579873ff95b31712e68770b800b1da6dba7b8
v.grausov <v.grausov@astondevs.ru>
2025-02-26 21:26:25 +0300

    fix ci test

--- src/test/java/ru/jpetsroretest/search/SearchProductTest.java
+++ src/test/java/ru/jpetsroretest/search/SearchProductTest.java
@@ -1,24 +1,34 @@
 package ru.jpetsroretest.search;
 
 import org.junit.jupiter.api.Disabled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.jpetsroretest.BaseUiTest;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.jpetsroretest.order.CreateOrderTest;
 
 @Feature("Проверка функциональности поиска")
 public class SearchProductTest extends BaseUiTest {
 
-    @Disabled
+    public static final Logger logger = LoggerFactory.getLogger(SearchProductTest.class);
+
+    @Test
     @Story("Пользователь может найти продукт")
     @DisplayName("Поиска продукта")
     public void searchProductsTest(){
+
+        logger.info("Написание в поисковую строку");
         baseRouter.searchPage()
                 .sendKeysKeyWordInputField("Bulldog");
+
+        logger.info("Нажатие на кнопку поиска");
         baseRouter.searchPage()
                 .clickSearchButton();
 
+        logger.info("Подтверждение нахождения продукта ");
         baseRouter.searchPage()
                 .successfullyFoundProductExist();
     }
